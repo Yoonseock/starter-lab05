3bdce7c2bcd462e61a39f513f4420dc831ec56e5
Yoonseock <yyoo7@my.bcit.ca>
2015-10-22 17:19:32 -0700

    do lab 5b

---
+++ application/views/quote_edit.php
@@ -0,0 +1,12 @@
+<div class="row">
+ <div class="errors">{message}
+</div>
+<div class="row">
+    <form action="/admin/confirm" method="post">
+        {fid}
+        {fwho}
+        {fmug}
+        {fwhat}
+        {fsubmit}
+    </form>
+</div>
